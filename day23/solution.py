81c032050207ead8648cc9ba33e64be8f3014f5b
Chad Bryant <chad.bryant@benefitfocus.com>
2023-11-27 23:22:48 -0500

    setup daily solutions

---
+++ day23/solution.py
@@ -0,0 +1,22 @@
+from aocd import get_data
+from aocd import submit
+
+def parse(puzzle_input):
+    """Parse input."""
+    return [line for line in puzzle_input.split(sep='\n')]
+    
+def part1(input):
+    """Implementation for part 1"""
+    pass
+    
+def part2(input):
+    """Implementation for part 2"""
+    pass
+
+if __name__ == "__main__":
+    year = 2023
+    day = 23
+    puzzle_input = get_data(day=day, year=year)        
+    input = parse(puzzle_input)
+    submit(part1(input), part="a", day=day, year=year)
+    #submit(part2(input), part="b", day=day, year=year)
