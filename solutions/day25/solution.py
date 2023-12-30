d382fde1e2ea0c99c6af37a1511549c6386ddb1f
Chad Bryant <chad.bryant@benefitfocus.com>
2023-12-30 16:40:42 -0500

    clean up

--- solutions/day25/solution.py
+++ solutions/day25/solution.py
@@ -2,46 +2,6 @@ import random
 import os
 import networkx as nx 
 
-# def karger_min_cut(graph):
-#     while len(graph) > 2:
-#         node1, node2 = random.choice(list(graph.items()))
-#         graph[node1].extend(graph[node2])
-#         for node in graph[node2]:
-#             graph[node].remove(node2)
-#             graph[node].append(node1)
-#         del graph[node2]
-#     return len(list(graph.values())[0])
-
-# def find_min_cut(graph, iterations):
-#     min_cut = float('inf')
-#     for _ in range(iterations):
-#         new_graph = defaultdict(list, {k: v[:] for k, v in graph.items()})
-#         cut = karger_min_cut(new_graph)
-#         min_cut = min(min_cut, cut)
-#     return min_cut
-
-# cuts=[]
-# def kargerMinCut(graph):
-#     while len(graph) > 2:
-#          v = random.choice(list(graph.keys()))
-#          w = random.choice(graph[v])
-#          contract(graph, v, w)
-#     mincut = len(graph[list(graph.keys())[0]])
-#     cuts.append(mincut)
-#     return mincut
-
-# def contract(graph, v, w):
-#     if w not in graph:
-#         return
-#     for node in graph[w]:  # merge the nodes from w to v
-#          if node != v:  # we dont want to add self-loops
-#              graph[v].append(node)
-#          if node in graph and w in graph[node]:
-#             graph[node].remove(w)  # delete the edges to the absorbed 
-#          if node in graph and node != v:
-#               graph[node].append(v)
-#     del graph[w]  # delete the absorbed vertex 'w'
-
 def parse(puzzle_input):
     """Parse input."""
     return [line for line in puzzle_input.split(sep='\n')]
